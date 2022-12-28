5f14e74c62086c5dd0cff50a671a547eb91d8c9b
Jordan <84494061+jnloves@users.noreply.github.com>
2022-12-28 12:26:10 -0500

    Create 1962.py

---
+++ 1962.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minStoneSum(self, piles: List[int], k: int) -> int:
+        piles = [-pile for pile in piles]
+        heapq.heapify(piles)
+
+        def heapsort(iterable):
+            h = []
+            for value in iterable:
+                heappush(h, value)
+            return [heappop(h) for i in range(len(h))]
+
+        piles = heapsort(piles)
+
+        for i in range(k):
+            smallest = piles[0]
+            heapq.heapreplace(piles, smallest + (floor(-smallest / 2)))
+
+        stones = 0
+        for item in piles:
+            stones += item
+        return -stones
